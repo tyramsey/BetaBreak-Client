084c7719ec10ac4c3f8d151578b5bcce74b72ad0
Ty Ramsey <tydram@gmail.com>
2021-05-08 14:31:25 -0400

    backwards working

--- src/components/CreateGoal.tsx
+++ src/components/CreateGoal.tsx
@@ -11,7 +11,6 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
 import APIURL from '../helpers/environments';
-
 export interface CreateGoalProps {
     sessionToken: string
 }
