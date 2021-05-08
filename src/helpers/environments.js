fdf48a0b95056152bd79d993c60fc3b1d5138c14
Ty Ramsey <tydram@gmail.com>
2021-05-08 13:38:33 -0400

    modify helper file

--- src/helpers/environments.js
+++ src/helpers/environments.js
@@ -4,8 +4,8 @@ switch (window.location.hostname){
     case 'localhost' ||'127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-    case 'tr-betabreak.herokuapp.com/.git':
-        APIURL ='https://betabreak.herokuapp.com/';
+    case 'tr-betabreak.herokuapp.com':
+        APIURL ='https://betabreak.herokuapp.com';
 }
 
 export default APIURL;
